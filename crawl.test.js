435ad6e764421c9fc334b49fcdcf40a3092ddae6
Bryant Bourgeois <bryant.bourgeois@dismylab.com>
2023-11-23 15:27:56 -0700

    lazy commit message

--- crawl.test.js
+++ crawl.test.js
@@ -1,74 +1,100 @@
-const {test, expect} = require('@jest/globals')
-const {JSDOM} = require('jsdom')
-const {normalizeURL} = require('./crawl.js')
-const {getURLsFromHTML} = require('./crawl.js')
+const { test, expect } = require("@jest/globals");
+const { JSDOM } = require("jsdom");
+const { normalizeURL } = require("./crawl.js");
+const { getURLsFromHTML } = require("./crawl.js");
 
-test('normalizeUrl test case 1', () => {
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+test("normalizeUrl test case 1", () => {
+  expect(normalizeURL("https://blog.boot.dev/path/")).toBe(
+    "blog.boot.dev/path"
+  );
 });
 
-test('normalizeUrl test case 2', () => {
-    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+test("normalizeUrl test case 2", () => {
+  expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path");
 });
 
-test('normalizeUrl test case 3', () => {
-    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+test("normalizeUrl test case 3", () => {
+  expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
 });
 
-test('normalizeUrl test case 4', () => {
-    expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+test("normalizeUrl test case 4", () => {
+  expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path");
 });
 
-test('normalizeUrl test case 5', () => {
-    expect(normalizeURL('https://www.youtube.com/watch?v=2JYT5f2isg4&t=221s')).toBe('www.youtube.com/watch');
+test("normalizeUrl test case 5", () => {
+  expect(
+    normalizeURL("https://www.youtube.com/watch?v=2JYT5f2isg4&t=221s")
+  ).toBe("www.youtube.com/watch");
 });
 
-test('normalizeUrl test case 6', () => {
-    expect(normalizeURL('https://jestjs.io/docs/getting-started')).toBe('jestjs.io/docs/getting-started');
+test("normalizeUrl test case 6", () => {
+  expect(normalizeURL("https://jestjs.io/docs/getting-started")).toBe(
+    "jestjs.io/docs/getting-started"
+  );
 });
 
-test('getURLsFromHTML absolute urls', () => {
-    expect(getURLsFromHTML(
-        `
+test("getURLsFromHTML absolute urls", () => {
+  expect(
+    getURLsFromHTML(
+      `
         <html>
             <body>
             <a href="https://blog.boot.dev">some innertext</a>
 </body>
         </html>
-        `, 'https://blog.boot.dev'
-    )).toEqual(['https://blog.boot.dev/']);
+        `,
+      "https://blog.boot.dev"
+    )
+  ).toEqual(["https://blog.boot.dev/"]);
 });
 
-test('getURLSFromHTML relative urls', () => {
-    expect(getURLsFromHTML(`
+test("getURLSFromHTML relative urls", () => {
+  expect(
+    getURLsFromHTML(
+      `
     <html>
         <body>
         <a href="/path/">some innertext</a>
 </body>
     </html> 
-    `, 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev/path/'])
-})
-test('getURLSFromHTML absolute and relative urls', () => {
-    expect(getURLsFromHTML(`
+    `,
+      "https://blog.boot.dev"
+    )
+  ).toEqual(["https://blog.boot.dev/path/"]);
+});
+test("getURLSFromHTML absolute and relative urls", () => {
+  expect(
+    getURLsFromHTML(
+      `
     <html>
         <body>
             <a href="/path1/">some innertext</a>
             <a href="https://blog.boot.dev/path2/">some innertext</a>
         </body>
     </html> 
-    `, 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/'])
-})
-test('getURLSFromHTML invalid link only', () => {
-    expect(getURLsFromHTML(`
+    `,
+      "https://blog.boot.dev"
+    )
+  ).toEqual(["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]);
+});
+test("getURLSFromHTML invalid link only", () => {
+  expect(
+    getURLsFromHTML(
+      `
     <html>
         <body>
             <a href="invalid">some innertext</a>
         </body>
     </html> 
-    `, 'https://blog.boot.dev')).toEqual([])
-})
-test('getURLSFromHTML invalid link with relative and absolute', () => {
-    expect(getURLsFromHTML(`
+    `,
+      "https://blog.boot.dev"
+    )
+  ).toEqual([]);
+});
+test("getURLSFromHTML invalid link with relative and absolute", () => {
+  expect(
+    getURLsFromHTML(
+      `
     <html>
         <body>
             <a href="invalid">some innertext</a>
@@ -76,5 +102,8 @@ test('getURLSFromHTML invalid link with relative and absolute', () => {
             <a href="https://blog.boot.dev/path2/">some innertext</a>
         </body>
     </html> 
-    `, 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/'])
-})
+    `,
+      "https://blog.boot.dev"
+    )
+  ).toEqual(["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]);
+});
